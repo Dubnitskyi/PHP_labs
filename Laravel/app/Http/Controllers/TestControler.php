41569cfc63d701164532aa39e0aea9f79af193bd
Ярослав Дубницький <ipz214_dyav@student.ztu.edu.ua>
2025-04-28 20:51:34 +0300

    add TestControler for laravel

---
+++ Laravel/app/Http/Controllers/TestControler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestControler extends Controller
+{
+    public function test()
+    {
+        return 'Hello from Laravel!';
+    }
+}
